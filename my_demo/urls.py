9e5ed5ce1487a90992eb4bcc29b2b8178c62f579
zlxy1 <yhz57654@163.com>
2026-07-23 17:47:40 +0800

    1.2

--- my_demo/urls.py
+++ my_demo/urls.py
@@ -1,9 +1,13 @@
 from . import views
 from django.urls import path
 
+from .views import staff_add
+
 urlpatterns = [
     path('', views.staff_list, name='staff_list'),
     path('staff/list', views.staff_list, name='staff_list'),
-    path('staff/add', views.staff_add, name='staff_add')
+    path('staff/add', views.staff_add, name='staff_add'),
+    path('staff/del/<int:pk>', views.staff_del, name='staff_del'),
+    path('staff/edit/<int:pk>',views.staff_edit,name='staff_edit')
 
 ]
--- my_demo/views.py
+++ my_demo/views.py
@@ -1,30 +1,61 @@
-
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from . import models
 from .models import Staff, work_status_choices, area_choice
 
-
-# Create your views here.
-def staff_list(request):
-    if request.method=='GET':
-        staff_list=Staff.objects.all()
-        return render(request,'staff/list.html',{'staff_list':staff_list})
-
 def staff_add(request):
-    if request.method=='GET':
-        ctx={'gender_list':models.gender_choice,
-             'staff_type_list':models.staff_type_choice,
-             'work_status_list':work_status_choices,
-             'area_choice_list':area_choice
-             }
-        return render(request,'staff/add.html',ctx)
-    if request.method=='POST':
+    if request.method == 'GET':
+        ctx = {'gender_list': models.gender_choice,
+               'staff_type_list': models.staff_type_choice,
+               'work_status_list': work_status_choices,
+               'area_choice_list': area_choice
+               }
+        return render(request, 'staff/add.html', ctx)
+    if request.method == 'POST':
         Staff.objects.create(name=request.POST.get('name'),
-        gender=request.POST.get('gender'),
-        birth=request.POST.get('birth'),
-        id_number=request.POST.get('id_number'),
-        staff_type=request.POST.get('staff_type'),
-        work_status=request.POST.get('work_status'),
-        work_area=request.POST.get('work_area'))
+                             gender=request.POST.get('gender'),
+                             birth=request.POST.get('birth'),
+                             id_number=request.POST.get('id_number'),
+                             staff_type=request.POST.get('staff_type'),
+                             work_status=request.POST.get('work_status'),
+                             work_area=request.POST.get('work_area'))
         return redirect('staff_list')
+
+
+def staff_del(request, pk):
+    staff_del = get_object_or_404(Staff, pk=pk)
+    if request.method == "POST":
+        staff_del.delete()
+        return redirect('staff_list')
+
+
+def staff_edit(request, pk):
+    staff = get_object_or_404(Staff, pk=pk)
+    ctx1 = {'gender_list': models.gender_choice,
+            'staff_type_list': models.staff_type_choice,
+            'work_status_list': work_status_choices,
+            'area_choice_list': area_choice,
+            'staff': staff
+            }
+    if request.method == 'GET':
+        return render(request, 'staff/edit.html', ctx1)
+    else:
+        staff.gender = request.POST.get('gender')
+        staff.birth = request.POST.get('birth')
+        staff.id_number = request.POST.get('id_number')
+        staff.staff_type = request.POST.get('staff_type')
+        staff.work_status = request.POST.get('work_status')
+        staff.work_area = request.POST.get('work_area')
+        staff.save()
+        return redirect('staff_list')
+
+
+def staff_list(request):
+    qs=Staff.objects.all()
+    serach_name=request.GET.get('name','')
+    serach_work_area=request.GET.get('work_area','')
+    if serach_name:
+        qs=qs.filter(name__icontains=serach_name)
+    if serach_work_area:
+        qs=qs.filter(name__icontains=serach_work_area)
+    return render(request,'staff/list.html',{'staff_list':qs})
 
