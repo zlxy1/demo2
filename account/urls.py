a9ccb339d26f74d99f2a03be2e44aff5d9bfdd88
zlxy1 <yhz57654@163.com>
2026-07-24 15:56:27 +0800

    1.4

--- account/urls.py
+++ account/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns=[
     path('login/',views.login_view,name='login_view'),
-    # path('/logout',views.logout_view,name='logout_view')
+    path('/logout',views.logout_view,name='logout_view')
 ]
