cf95c1d752a56d745d2927d6141ade640cdccc05
zlxy1 <yhz57654@163.com>
2026-07-28 17:45:00 +0800

    1.7

--- account/models.py
+++ account/models.py
@@ -1,3 +1,6 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class UserProfile(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE,related_name='profile')
+    avatar=models.ImageField(upload_to='avatar/', default='avatar/default.png', blank=True)
